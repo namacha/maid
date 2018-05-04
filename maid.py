5fef114433e684a97b703e45cc22713126a8bf56
ayu <mac.ayu15@gmail.com>
2018-05-04 17:04:06 +0900

    added archetype

---
+++ maid.py
@@ -0,0 +1,17 @@
+from queue import Queue
+
+
+class Chore:
+    
+    def __init__(self):
+        pass
+
+
+class Maid:
+    
+    def __init__(self):
+        self.name = ''
+        self.todos = Queue()
+    
+    def do_job(self):
+        pass
