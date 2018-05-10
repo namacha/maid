09d9582527fa056db7340d60475d4539d115c7f9
ayu <mac.ayu15@gmail.com>
2018-05-10 21:03:16 +0900

    sketch

--- maid.py
+++ maid.py
@@ -2,17 +2,19 @@ import time
 
 import redis
 from flask import Flask, make_response, jsonify, request
-from queue import Queue
+from j9 import Queue
+
+from maid_tasks.tasks import TASKS
 
 
 app = Flask(__name__)
 queue = Queue()
 
 
-@api.route('/heartbeat', methods=['GET'])
+@app.route('/heartbeat', methods=['GET'])
 def heartbeat():
     result = {
-        'result': True,
+        'status': True,
         'message': 'Welcome home, Master!'
     }
     return make_response(jsonify(result))
@@ -21,4 +23,38 @@ def heartbeat():
 @app.route('/order', methods=['POST'])
 def order():
     action = request.json['action']
-    job_id = queue.enqueue
+    task = TASKS.get(action)
+    if task is None:
+        response = {
+            'status': False,
+            'message': 'Task not found',
+        }
+        return make_response(jsonify(response))
+    job_id = queue.enqueue(task)
+    response = {
+        'status': True,
+        'job_id': job_id,
+    }
+    return make_response(jsonify(response))
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    job_id = request.json['job_id']
+    r = queue.result.get(job_id)
+    if r is None:
+        response = {
+            'status': False,
+            'message': 'No such job_id',
+        }
+        return make_response(jsonify(response))
+    response = {
+        'status': True,
+        'result': r,
+    }
+    return make_response(jsonify(response))
+
+
+if __name__ == '__main__':
+    queue.start()
+    app.run('0.0.0.0', port=8080, debug=True)
